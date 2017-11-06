044381148a41c96dc631dce05918e61a48c38650
PoBlue <250299430@qq.com>
2017-11-06 16:37:27 +0800

    [css] new deck

--- components/newDeckView.js
+++ components/newDeckView.js
@@ -58,16 +58,19 @@ class NewDeckView extends Component {
 
     render() {
         return(
-            <KeyboardAvoidingView behavior="padding">
-                <Text>What is the title of your new deck?</Text>
+            <KeyboardAvoidingView style={styles.container} behavior="padding">
+                <Text style={styles.question}>What is the title of your new deck?</Text>
                 <TextInput 
+                    style={styles.inputFiled}
                     placeholder={"Input deck name"}
                     value={this.state.input}
                     onChangeText={(text) => this.handleTextChange(text)}
                 />
-                <Text>{this.state.errorMsg}</Text>
-                <Text>{this.state.sucessfulMsg}</Text>
-                <Button title="Submit" onPress={() => this.submit()}/>
+                <Text style={styles.errorText}>{this.state.errorMsg}</Text>
+                <Text style={styles.sucessfulText}>{this.state.sucessfulMsg}</Text>
+                <TouchableOpacity style={styles.submitButton} onPress={() => this.submit()}>
+                    <Text style={styles.sumbitText}>Submit</Text>
+                </TouchableOpacity>
             </KeyboardAvoidingView>
         )
     }
@@ -78,5 +81,49 @@ function mapStateToProps(decks){
         decks
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    description: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+        flex: 1,
+    },
+    inputFiled: {
+        fontSize: 30,
+        marginTop: 32,
+        marginBottom: 32,
+    },
+    question: {
+        fontSize: 40,
+        color: 'black',
+        fontWeight: 'bold'
+    }, 
+    submitButton: {
+        backgroundColor: 'black',
+        borderRadius: 10,
+        margin: 10,
+        padding: 5,
+        alignItems: 'center',
+    },
+    errorText: {
+        color: 'red'
+    },
+    sucessfulText: {
+        color: 'green'
+    },
+    sumbitText: {
+        fontSize: 18,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 25,
+        paddingRight: 25,
+        color: 'white',
+    },
+});
 
 export default connect(mapStateToProps)(NewDeckView)
